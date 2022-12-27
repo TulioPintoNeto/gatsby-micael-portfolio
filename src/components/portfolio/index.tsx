d49e194c6ac1196a862cf87229620f794809f9f1
Tulio Pinto Neto <tulio.neto@startaideia.com.br>
2022-12-27 23:42:32 +0000

    feat: Add second and third portfolio sections

--- src/components/portfolio/index.tsx
+++ src/components/portfolio/index.tsx
@@ -6,7 +6,7 @@ import { whiteDot } from "../../ui/white-dot.module.css";
 export default function Portfolio() {
     return (
         <section className={styles.portfolio}>
-            <div className={styles.portfolio__header}>
+            <header>
                 <div>
                     <h3>
                         <p>lastest</p>
@@ -20,13 +20,41 @@ export default function Portfolio() {
                     <div
                         className={`${whiteDot} ${styles.mediumOrHigherDevices}`}
                     />
-                    <p>
+                    <p className={styles.portfolio__paragraph}>
                         take a look in my most recent work, feel free <br /> to
                         share any thoughts and ask anything.
                     </p>
                 </div>
+            </header>
+            <section>
+                <img src="./photo-teste.jpg" />
+                <h4>Lounge</h4>
+                <p className={styles.portfolio__paragraph}>
+                    A solution for companies that seek to adapt their routines
+                    to the digital world.
+                </p>
+            </section>
+            <div className={styles.portfolio__row}>
+                <section>
+                    <img src="./photo-teste.jpg" />
+                    <h4>Startup Emotus</h4>
+                    <p className={styles.portfolio__paragraph}>
+                        Plataform developed to better organize budgets and
+                        inventory related to solar energy industry.
+                    </p>
+                </section>
+                <section>
+                    <div
+                        className={`${whiteDot} ${styles.mediumOrHigherDevices}`}
+                    />
+                    <img src="./photo-teste.jpg" />
+                    <h4>Cobalto</h4>
+                    <p className={styles.portfolio__paragraph}>
+                        A solution for companies that seek to adapt their
+                        routines to the digital world.
+                    </p>
+                </section>
             </div>
-            <img src="./photo-teste.jpg" />
         </section>
     );
 }
